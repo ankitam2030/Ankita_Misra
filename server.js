548a70b0b8ed88d7bc538418fb9889af747d32c2
ankitam2030 <84309296+ankitam2030@users.noreply.github.com>
2021-05-17 13:02:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.post("/",function(req,res){
   var n1=Number(req.body.num1);
   var n2=Number(req.body.num2);
   var result=n1+n2;
-  res.send("The result is "+result);
+  res.send("The answer is "+result);
 });
 
 app.listen(3000,function(){
